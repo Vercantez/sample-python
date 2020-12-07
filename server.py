3e80425cd5f6f76452e08f22ee4e32b4eb0e9a98
Miguel Salinas <mikeys@me.com>
2020-12-07 08:15:03 -0600

    decode

--- server.py
+++ server.py
@@ -14,15 +14,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         query_components = parse_qs(urlparse(self.path).query)
         print(query_components)
-        challenge = query_components["hub.challenge"] 
-        msg = '%s' % (challenge)
+        challenge = query_components["hub.challenge"]
         self.wfile.write(challenge[0].encode())
         print(self.request)
         
     def do_POST(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        print(self.rfile)
+        print(self.rfile.read().decode('utf-8'))
         print(self.request)
 
 
