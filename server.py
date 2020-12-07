4a3a7ab9bb0f4734da945174d28b60904ddd13f4
Miguel Salinas <mikeys@me.com>
2020-12-06 19:42:16 -0600

    tyj

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         msg = 'Hello! you requested %s' % (self.requestline)
         self.wfile.write(msg.encode())
-        print(self.requestline)
+        print(self.request)
 
 
 port = int(os.getenv('PORT', 80))
